86bf657bd80e217e5489d3f0d8bd084f5ef709b8
Ahmad <aahhmmaadd.bkr@gmail.com>
2024-07-29 18:22:40 +0500

    Ticket VBP-549

--- cypress/e2e/paymentsHistory.cy.js
+++ cypress/e2e/paymentsHistory.cy.js
@@ -45,55 +45,77 @@ describe('Payments History',function(){
         paymentsHistory.validatePaginationFilters('50 / page')
         paymentsHistory.validateRows(50)
     })
-    it('TC_PH_005 - Verify that by default there should be 20 recipents in Payment History page.', function(){
+     
+ 	
+it.only('TC_PH_005 - (VBP-549) The pagination filter is not working properly on payment history page', function(){
+    paymentspage.goToPaymentsDashborad()
+    paymentsHistory.goToPaymentsHistory()
+    paymentsHistory.validatePaginationFilters('10 / page')
+    paymentsHistory.validateRows(10)
+    cy.get('[data-row-key="2"] > :nth-child(3)').should('be.visible').click()
+    cy.get('[data-testid="container"] > .ant-card-body > :nth-child(1) > .ant-col > .ant-typography').should('be.visible').should('contain.text','Specific Payment History')
+    cy.wait(3000)
+    cy.go('back')
+    cy.wait(3000)
+    paymentsHistory.validateRows(10)
+    paymentsHistory.validatePaginationFilters('100 / page')
+    paymentsHistory.validateRows(100)
+    cy.get('[data-row-key="2"] > :nth-child(3)').should('be.visible').click()
+    cy.get('[data-testid="container"] > .ant-card-body > :nth-child(1) > .ant-col > .ant-typography').should('be.visible').should('contain.text','Specific Payment History')
+    cy.wait(3000)
+    cy.go('back')
+    cy.wait(3000)
+    paymentsHistory.validateRows(100)
+})
+    it('TC_PH_006 - Verify that by default there should be 20 recipents in Payment History page.', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.validateDefaultPaginationFilter('20 / page')
     })
-    it('TC_PH_006 - Verify that on clicking "Repeat" button, user navigates to New Payment page and is able to repeat it', function(){
+    it('TC_PH_007 - Verify that on clicking "Repeat" button, user navigates to New Payment page and is able to repeat it', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.validateRepeatBtn()
     })
-    it('TC_PH_007 - Verify that on clicking "Draft Payments" button, user navigates to Drafts Payment page', function(){
+    it('TC_PH_008 - Verify that on clicking "Draft Payments" button, user navigates to Drafts Payment page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
     })
-    it('TC_PH_008 - Verify that on clicking "Review" button, user navigates to Payments Summary page', function(){
+    it('TC_PH_009 - Verify that on clicking "Review" button, user navigates to Payments Summary page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.goToReviewPayments()
     })
-    it('TC_PH_009 - Verify that on clicking "Return to Draft Payments" button, user navigates to Draft Payments page', function(){
+    it('TC_PH_010 - Verify that on clicking "Return to Draft Payments" button, user navigates to Draft Payments page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.goToReviewPayments()
         paymentsHistory.goToDraftPaymentsDashboard()
     })
-    it('TC_PH_010 - Verify that on clicking "Delete Draft" button, user is able to delete it and navigates to Draft Payments page', function(){
+    it('TC_PH_011 - Verify that on clicking "Delete Draft" button, user is able to delete it and navigates to Draft Payments page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.goToReviewPayments()
         paymentsHistory.goToDeleteDreft()
     })
-    xit('TC_PH_011 - Verify that on clicking "Pay Recipient" button, user is able to delete it and navigates to Draft Payments page', function(){
+    xit('TC_PH_012 - Verify that on clicking "Pay Recipient" button, user is able to delete it and navigates to Draft Payments page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.goToReviewPayments()
         
     })
-    it('TC_PH_012 - Validate that the pagination is working fine on "Draft Payment" page', function(){
+    it('TC_PH_013 - Validate that the pagination is working fine on "Draft Payment" page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.validatePagination()
     })
-    it('TC_PH_013 - Validate the pagination filters work as intended on Draft Payments page', function(){
+    it('TC_PH_014 - Validate the pagination filters work as intended on Draft Payments page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
@@ -101,42 +123,42 @@ describe('Payments History',function(){
         paymentsHistory.validateRows(10)
         paymentsHistory.validatePaginationFilters('50 / page')
     })
-    it('TC_PH_014 - Verify that by default there should be 10 recipents in Draft Payments page.', function(){
+    it('TC_PH_015 - Verify that by default there should be 10 recipents in Draft Payments page.', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.validateDefaultPaginationFilter('10 / page')
     })
-    it('TC_PH_015 - Verify that on clicking "Payment History" button present on Draft Payments page, the user is bale to navigate to "Payment History" page', function(){
+    it('TC_PH_016 - Verify that on clicking "Payment History" button present on Draft Payments page, the user is bale to navigate to "Payment History" page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.goToPaymentsHistoryBtn()
     })
-    it('TC_PH_016 - Verify that Search Bar present on the Draft Payments page is working fine', function(){
+    it('TC_PH_017 - Verify that Search Bar present on the Draft Payments page is working fine', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToDraftPayments()
         paymentsHistory.validateSearchBar('Hamza')
     })
-    it('TC_PH_017 - Verify that on clicking "Manual Trade/Payments History" button, the user is bale to navigate to "Manual Trade History" page', function(){
+    it('TC_PH_018 - Verify that on clicking "Manual Trade/Payments History" button, the user is bale to navigate to "Manual Trade History" page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToManualTrade()
     })
-    it('TC_PH_018 - Verify that Search Bar present on "Manual Trade/Payments History" is working fine', function(){
+    it('TC_PH_019 - Verify that Search Bar present on "Manual Trade/Payments History" is working fine', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToManualTrade()
         paymentsHistory.validateSearchBar('Account')
     })
-    it('TC_PH_019 - Validate that the pagination is working fine on "Manual Trade/Payments History" page', function(){
+    it('TC_PH_020 - Validate that the pagination is working fine on "Manual Trade/Payments History" page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToManualTrade()
         paymentsHistory.validatePagination()
     })
-    it('TC_PH_020 - Validate the pagination filters work as intended on "Manual Trade/Payments History" page', function(){
+    it('TC_PH_021 - Validate the pagination filters work as intended on "Manual Trade/Payments History" page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToManualTrade()
@@ -145,19 +167,19 @@ describe('Payments History',function(){
         paymentsHistory.validatePaginationFilters('50 / page')
         paymentsHistory.validateRows(50)
     })
-    it('TC_PH_021 - Verify that by default there should be 20 recipents in "Manual Trade/Payments History" page.', function(){
+    it('TC_PH_022 - Verify that by default there should be 20 recipents in "Manual Trade/Payments History" page.', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToManualTrade()
         paymentsHistory.validateDefaultPaginationFilter('20 / page')
     })
-    it('TC_PH_022 - Verify that by clicking on a recipient in "Manual Trade/Payments History", the user navigates to Specific Manual Trade History page', function(){
+    it('TC_PH_023 - Verify that by clicking on a recipient in "Manual Trade/Payments History", the user navigates to Specific Manual Trade History page', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToManualTrade()
         paymentsHistory.goToSpecificManualTrade()
     })
-    it('TC_PH_023 - Verify that by clicking on "Download PDF Statement" in "Manual Trade/Payments History" page, the user is able to download it', function(){
+    it('TC_PH_024 - Verify that by clicking on "Download PDF Statement" in "Manual Trade/Payments History" page, the user is able to download it', function(){
         paymentspage.goToPaymentsDashborad()
         paymentsHistory.goToPaymentsHistory()
         paymentsHistory.goToManualTrade()
